af7383a9848b2b6c4e41d703331d3a31bf53b2e0
Matnazar <matnazarmatnazarov3@gmail.com>
2026-01-13 23:45:02 +0500

    feat: 25-dars script.js yaratish - asosiy integratsiya script

---
+++ 2-modul-advanced-javascript/25-amaliyot-date-va-loader/script.js
@@ -0,0 +1,9 @@
+// Dars 25: Amaliyot. Date va Loader - Asosiy integratsiya script
+
+console.log('📚 Dars 25: Amaliyot. Date va Loader');
+console.log('Bu darsda quyidagi amaliy loyihalar o\'rganiladi:');
+console.log('1. Date loyihasi - Vaqt ko\'rsatish, countdown timer');
+console.log('2. Loader loyihasi - Loading states, spinner animations');
+console.log('');
+console.log('Demo maydoni tayyor!');
+console.log('Har bir loyihani yuklash uchun yuqoridagi tugmalarni bosing.');
