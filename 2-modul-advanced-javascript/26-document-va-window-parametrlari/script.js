ce62eaef794a0ff417ff2da329747a5e6eb5d0bb
Matnazar <matnazarmatnazarov3@gmail.com>
2026-01-14 23:14:29 +0500

    feat: 26-dars script.js yaratish - asosiy integratsiya script

---
+++ 2-modul-advanced-javascript/26-document-va-window-parametrlari/script.js
@@ -0,0 +1,9 @@
+// Dars 26: Document va Window Parametrlari - Asosiy integratsiya script
+
+console.log('📚 Dars 26: Document va Window Parametrlari');
+console.log('Bu darsda quyidagi mavzular o\'rganiladi:');
+console.log('1. Document parametrlari - document.documentElement, document.body, document.head');
+console.log('2. Window parametrlari - window.innerWidth, window.location, window.history');
+console.log('3. Screen parametrlari - screen.width, screen.height, screen.colorDepth');
+console.log('');
+console.log('Har bir mavzu uchun yuqoridagi tugmalarni bosing.');
